653dad09490410a5e14b7582ddaf1e2f9c45de97
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-12-15 23:00:43 +0100

    updated terraform version

--- azure_vpn_application/versions.tf
+++ azure_vpn_application/versions.tf
@@ -2,19 +2,13 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 3"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.13.1"
+      version = "~> 3.7"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3"
+      version = "~> 3.7"
     }
   }
+
+  required_version = ">= 1.10.0"
 }
